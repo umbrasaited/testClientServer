84241fc3e16cdc19c8e42f37dcba1b4a34ae889e
Deividas Januska <dj9909@gmail.com>
2018-11-02 14:14:48 +0200

    perduodu Dovilei

--- db.js
+++ db.js
@@ -1,7 +1,39 @@
 module.exports = class db {
+  //sitoj funkcijoj reikia susijungt su duombaze ir surasyt visokius nustatymus, jeigu reikia
   init() {
     console.log("database initialized!");
   }
-  confirmLogin(data) {}
-  createNewUser(data) {}
+
+  //return 0 - successful login
+  //return
+  //data format:              data{username, password, confirmPassword}
+  confirmLogin(data) {
+    //is duombazes reik istraukt prisijungimo duomenis ir patikrint, ar sutampa su naudotojo ivestais duomenim ("data")
+    //jei prisijungimas sekmingas, funkcija grazina 0, jei ne - 1 ar koki kitoki errorCode, kuri nusistatysi kaip nori. svarbu pazymek komentaruose, ka kas reiskia
+  }
+
+  //creates new user if username and password are valid
+  //return 0 - successful creation
+  //return 1 - username too short (min 6 characters)
+  //return 2 - password too short (min 8 characters)
+  //return 3 - password confirmPassword doesnt match
+  //return 4 - username taken
+  //data format:              data{username, password, confirmPassword}
+  createNewUser(data) {
+    if (data[0].length > 5 && data[1].length > 7 && data[1] == data[2]) {
+      //TODO: reikia parasyt kodo dali, kuri patikrintu ar username'as duombazej yra ar ne (jei yra => return 4;)
+
+      //if program reaches this line of code, username was successfully created
+      //duomenys permetami i duombaze
+      return 0;
+    } else {
+      if (data[0].length < 6) {
+        return 1;
+      } else if (data[1] < 8) {
+        return 2;
+      } else if (data[1] != data[2]) {
+        return 3;
+      }
+    }
+  }
 };
--- index.js
+++ index.js
@@ -5,12 +5,60 @@ var io = require("socket.io")(http);
 var db = require("./db");
 
 var dataBase = new db();
+
+//sita kodo dalis veiks visada, kada paleisi programa. this means, kad sitoj vietoj gali per konsoles langa susivest/isvest duomenis.
 http.listen(3000, function() {
   console.log("listening on *:3000");
-  dataBase.init();
+  //Dovile, kolkas tau uzteks pasitestuot viska sitoj vietoj
+  //kai pasikursi funkcijas db.js faile, testuok jas rasydama:
+  //dataBase.init();
+  //database.createNewUser(duomenys);
+  //database.confirmLogin(duomenys);
+  //"duomenys" turetu but javascript objektas {property1: value1, property2: value2, property3: value3}
+  //musu atveju pavyzdys: {username: "user1", password: "pass", confirmPassword: "pass"}
+  //log in funkcijai reikes tik username ir password
+  //funkcijos grazint turetu reiksmes, pagal kurias pranesim client'ui, ar pavyko prisiregistruot ir prisijungt ar ne (kaip tai atrodo gali pamatyt apacioj)
+  //
 });
 
+//sita kodo dalis neveiks, kol neprisijungsi prie serverio adresu IP:3000 (tau jos kaip ir nereik kolkas)
 io.on("connection", function(socket) {
   console.log("a user connected");
-  socket.on("sign_up", data => {});
+
+  socket.on("sign-up", data => {
+    let errCode = dataBase.createNewUser(data);
+    if (!errCode) {
+      socket.emit("successful-sign-up");
+    } else {
+      switch (errCode) {
+        case 1:
+          socket.emit("username-too-short");
+        case 2:
+          socket.emit("password-too-short");
+        case 3:
+          socket.emit("password-not-match");
+        case 4:
+          socket.emit("username-taken");
+      }
+    }
+  });
+
+  socket.on("log-in", data => {
+    let success = dataBase.confirmLogin(data);
+    if (success) {
+      socket.emit("login-success");
+    } else {
+      socket.emit("login-failed");
+    }
+  });
+});
+
+socket.emit("loginas", {
+  username: "deividas",
+  password: "nanana",
+  confirm: "nanana"
+});
+
+socket.on("loginas", function(data) {
+  //asdasdasdasd
 });
